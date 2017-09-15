8570f797a6580e83d8a678a1f5b7f06e2c46bc82
pascal <pascalzontrop@gmail.com>
2017-09-15 19:15:50 +0200

    commit forgotten files

---
+++ src/app/services/hero/hero.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@angular/core';
+import {Hero} from '../../model/hero';
+import { HEROES} from '../../mock-heroes';
+
+@Injectable()
+export class HeroService {
+
+  getHeroes(): Promise<Hero[]> {
+    return Promise.resolve(HEROES);
+  }
+
+}
